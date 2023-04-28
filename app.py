6cf0712351e46d6ca294626b762ac9570b1d39ff
Roshan Tiwari <roshan.tiwari.24.5.2001@gmail.com>
2023-04-28 10:00:30 +0530

    updated

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ with z2:
 
 
 selected = option_menu(menu_title=  None, 
-                            options = ['Expense' , 'Income' , 'About'],
-                            icons= ['graph-up' , 'reception-4', 'person-lines-fill'],
+                            options = ['Expense' , 'Income' ],
+                            icons= ['graph-up' , 'reception-4'],
                             orientation= "horizontal", )
 
 def load_lottieurl(url: str):
@@ -34,7 +34,7 @@ lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_M9p
 
 @st.cache(allow_output_mutation=True)
 def read_csv():
-    data = pd.read_csv("Dataset/personal_transactions.csv")
+    data = pd.read_csv("C:/Users/91845/Downloads/personal_transactions.csv")
     data.rename(columns={'Transaction Type': 'Transaction_Type', 'Account Name': 'Account_Name'}, inplace=True)
     return data
 
@@ -121,7 +121,8 @@ if selected == 'Expense':
             fig = px.histogram(exp_data, x='Amount', y='Category', color_discrete_sequence= ['orange'] ,  barmode='group' , width= 800 )
             fig.update_xaxes(showgrid=False, zeroline=False)
             fig.update_yaxes(showgrid=False, zeroline=False)
-            fig.update_layout(title_text= "Total Expenses" ,title_x =  0.5 , title_font_color = 'red')
+            fig.update_layout(title_text= "Total Expenses" ,title_x =  0.5 , title_font_color = 'red' , plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
+
             st.plotly_chart(fig , config = {'displayModeBar' : False})
             
 
@@ -143,7 +144,7 @@ if selected == 'Expense':
             fig8 = px.bar(data_frame=debit_avg, x=debit_avg['Category'], y= debit_avg['Avarage'] , width = 800 , height = 500)
             fig8.update_xaxes(showgrid=False)
             fig8.update_yaxes(showgrid=False)
-            fig8.update_layout(title_text="Avarage spend on category", title_x=0.5, title_font_color='red')
+            fig8.update_layout(title_text="Avarage spend on category", title_x=0.5, title_font_color='red' , plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
             st.plotly_chart(fig8 , config = {'displayModeBar' : False})
 
     with st.container():
@@ -154,7 +155,7 @@ if selected == 'Expense':
             fig8 = px.line(data_frame=debit_avg, x='month_year', y='Total' , color_discrete_sequence= ['red'])
             fig8.update_xaxes(showgrid=False)
             fig8.update_yaxes(showgrid=False)
-            fig8.update_layout(title_text="Monthly Spend", title_x=0.5, title_font_color='red')
+            fig8.update_layout(title_text="Monthly Spend", title_x=0.5, title_font_color='red' , plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
             st.plotly_chart(fig8 , config = {'displayModeBar' : False})
 
 
@@ -191,42 +192,42 @@ if selected == 'Income':
 
 
     l1, l2, l3, l4 , l5 = st.columns([2,2,2,2,2])
+    with st.container():
+        with l1:
+            tot_spend = data[data["Transaction_Type"] == "debit"]
+            tot_spend = tot_spend.groupby(["Transaction_Type"])["Amount"].sum().reset_index(name='Total')
+            fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_spend._get_value(0, 'Total')) , title= {'text' : 'Total Spend'}))
+            fig1.update_layout( width = 350 , height = 350)
+            st.write(fig1)
 
-    with l1:
-        tot_spend = data[data["Transaction_Type"] == "debit"]
-        tot_spend = tot_spend.groupby(["Transaction_Type"])["Amount"].sum().reset_index(name='Total')
-        fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_spend._get_value(0, 'Total')) , title= {'text' : 'Total Spend'}))
-        fig1.update_layout( width = 350 , height = 350)
-        st.write(fig1)
-
-
-    with l2 :
-        tot_inc = data[data["Transaction_Type"] == "credit"]
-        tot_inc = tot_inc.groupby(["Transaction_Type"])["Amount"].sum().reset_index(name='Total')
-        fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_inc._get_value(0, 'Total')) , title= {'text' : 'Total Income'}))
-        fig1.update_layout( width = 350 , height = 350)
-        st.write(fig1)
+    with st.container():
+        with l2 :
+            tot_inc = data[data["Transaction_Type"] == "credit"]
+            tot_inc = tot_inc.groupby(["Transaction_Type"])["Amount"].sum().reset_index(name='Total')
+            fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_inc._get_value(0, 'Total')) , title= {'text' : 'Total Income'}))
+            fig1.update_layout( width = 350 , height = 350)
+            st.write(fig1)
         
-
-    with l3:
-        tot_avg = data[data["Transaction_Type"] == "debit"]
-        tot_avg = tot_avg.groupby(["Transaction_Type"])["Amount"].mean().reset_index(name='Total')
-        fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_avg._get_value(0, 'Total')) , title= {'text' : 'Avarage Spend'}))
-        fig1.update_layout( width = 350 , height = 350)
-        st.write(fig1)
-
-    with l4:
-        tot_avg_in = data[data["Transaction_Type"] == "credit"]
-        tot_avg_in = tot_avg_in.groupby(["Transaction_Type"])["Amount"].mean().reset_index(name='Total')
-        fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_avg_in._get_value(0, 'Total')) , title= {'text' : 'Average Income'}))
-        fig1.update_layout( width = 350 , height = 350)
-        st.write(fig1)
-
-    with l5:
-        total_savings = round(((tot_inc._get_value(0, 'Total')-(tot_spend._get_value(0 , 'Total')))),3)
-        fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= total_savings , title= {'text' : 'Total Saving'}))
-        fig1.update_layout( width = 350 , height = 350)
-        st.write(fig1)
+    with st.container():
+        with l3:
+            tot_avg = data[data["Transaction_Type"] == "debit"]
+            tot_avg = tot_avg.groupby(["Transaction_Type"])["Amount"].mean().reset_index(name='Total')
+            fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_avg._get_value(0, 'Total')) , title= {'text' : 'Avarage Spend'}))
+            fig1.update_layout( width = 350 , height = 350)
+            st.write(fig1)
+    with st.container():
+        with l4:
+            tot_avg_in = data[data["Transaction_Type"] == "credit"]
+            tot_avg_in = tot_avg_in.groupby(["Transaction_Type"])["Amount"].mean().reset_index(name='Total')
+            fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= (tot_avg_in._get_value(0, 'Total')) , title= {'text' : 'Average Income'}))
+            fig1.update_layout( width = 350 , height = 350)
+            st.write(fig1)
+    with st.container():
+        with l5:
+            total_savings = round(((tot_inc._get_value(0, 'Total')-(tot_spend._get_value(0 , 'Total')))),3)
+            fig1 = go.Figure(go.Indicator(mode = 'gauge + number' , value= total_savings , title= {'text' : 'Total Saving'}))
+            fig1.update_layout( width = 350 , height = 350)
+            st.write(fig1)
 
 
     st.markdown(f'<h1 style = " text-align : center ; color: green;font-size:35px;">{"Income Analysis"}</h1>', unsafe_allow_html=True)
@@ -238,7 +239,7 @@ if selected == 'Income':
             credit_data = data.groupby(['Transaction_Type'])
             credit_data = credit_data.get_group('credit')
             fig3 = px.pie(credit_data, names='Account_Name', values='Amount' , width= 600 )
-            fig3.update_layout(title_text="Mode of Payment", title_x=0.3, title_font_color='green')
+            fig3.update_layout(title_text="Mode of Payment", title_x=0.3, title_font_color='green', plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
             st.plotly_chart(fig3 , config = {'displayModeBar' : False})
             
     with st.container():
@@ -248,7 +249,7 @@ if selected == 'Income':
             fig8 = px.bar(data_frame=credit_avg, x='month_year', y='Total' , color_discrete_sequence= ['lime'])
             fig8.update_xaxes(showgrid=False)
             fig8.update_yaxes(showgrid=False)
-            fig8.update_layout(title_text="Total Income", title_x=0.5, title_font_color='green')
+            fig8.update_layout(title_text="Total Income", title_x=0.5, title_font_color='green', plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
             st.plotly_chart(fig8 , config = {'displayModeBar' : False})
             
 
@@ -261,7 +262,7 @@ if selected == 'Income':
 
             fig4.update_xaxes(showgrid = False)
             fig4.update_yaxes(showgrid=False)
-            fig4.update_layout(title_text="Income vs Expense", title_x=0.5, title_font_color='green')
+            fig4.update_layout(title_text="Income vs Expense", title_x=0.5, title_font_color='green',plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)))
             st.plotly_chart(fig4 , config = {'displayModeBar' : False})
 
         with c1:
@@ -270,122 +271,6 @@ if selected == 'Income':
             fig8 = px.line(data_frame=debit_avg, x='month_year', y='Total' , color_discrete_sequence= ['blue'])
             fig8.update_xaxes(showgrid=False)
             fig8.update_yaxes(showgrid=False)
-            fig8.update_layout(title_text="Avarage Monthly Income", title_x=0.5, title_font_color='green')
+            fig8.update_layout(title_text="Avarage Monthly Income", title_x=0.5, title_font_color='green', plot_bgcolor = "rgba(0,0,0,0)" , yaxis = (dict(showgrid = False)) )
             st.plotly_chart(fig8 , config = {'displayModeBar' : False})
-
-if selected == 'About':
-    with st.container():
-        l1 , l2 = st.columns((3,2))
-        with l1:
-            st.markdown('##            ')
-            st.markdown('#####            ')
-            st.markdown("### Hello and Welcome to my Personal Finance Management")
-            st.markdown("#### I am Roshan :wave:, A Data science student from R. J. College.")
-            st.markdown("###### In this project i have analysed a persons personal transaction data over a period of time.\n ###### There is detailed visulization of " + f'<h style = " color: Red;font-size:20px;">{"Expense"}</h>'+ " , " + f'<h style = "color: green;font-size:20px;">{"Income"}</h>' + '  And  ' + f'<h style = "color: blue;font-size:20px;">{"Category"}</h>' ""+"\n ###### This project will help the user to understand and track there expense for better investments and savinges. \n ###### i have created a detailed guide below on how to use this dashboard. ", unsafe_allow_html=True)
-    with st.container():
-        with l2:
-            st_lottie(
-        lottie_hello,
-        speed=1,
-        reverse=False,
-        loop=True,
-        quality="low", # medium ; high
-        height=300,
-        width=300,
-        key=None,)
-            
-    st.write('---')
-
-    st.markdown('### Guide for the Dashboard')
-    with st.container():
-        x1 , x2 , x3 = st.columns([2,1,4])
-        with x1:
-            image1 = Image.open('images/filter.png')
-            st.image(image1 , width= 600)
-
-        with x3:
-            st.markdown('###')
-            st.markdown('#### How can you filter the data in this dashboard...')
-            st.markdown('##### When user open the dashbord there will be a sidebar with filters.')
-            st.markdown('##### This page will open by default, user can click on cross button to close filter section.')
-            st.markdown('##### User can filter the data based on Category, Month and year.')
-            st.markdown("##### For more you can watch the video on how to filter data.. \n [watch video...]()")
-
-    st.write('---')
-    with st.container():
-        x1 , x2 , x3 = st.columns([2,1,4])
-        with x1:
-            image1 = Image.open('images/expanes.png')
-            st.image(image1 , width= 600)
-
-        with x3:
-            st.markdown('###')
-            st.markdown('#### How to Track your Expense in this Dashboard...')
-            st.markdown('##### There are some major KPI and some Visulization of expense over time')
-            st.markdown('##### On the first bar chart total expanes over different categorty is ploted on the other side there is a pie chart on account type.')
-            st.markdown('##### scroll down there is a line chart on month wise spend and a bar chart on avarage spend on category. For more you can watch the video on how to filter data.. \n [watch video...]()')
-    st.write('---')
-    
-    with st.container():
-        x1 , x2 , x3 = st.columns([2,1,4])
-        with x1:
-            image1 = Image.open('images/income.png')
-            st.image(image1 , width= 600)
-
-        with x3:
-            st.markdown('###')
-            st.markdown('#### How to Track your Income in this Dashboard...')
-            st.markdown('##### There are some major KPI and some Visulization of expense over time')
-            st.markdown('##### On the first bar chart total income for different months is ploted on the other side there is a pie chart on account type.')
-            st.markdown('##### scroll down there is a line chart on avarage income for all months  and a bar chart for comparinsion of Expense and Income. For more you can watch the video on how to filter data.. \n [watch video...]()')
-    st.write('---')
-    contact_form = """
-            <form action="https://formsubmit.co/roshan.tiwari.24.5.2001@gmail.com" method="POST">
-                <input type="hidden" name="_captcha" value="false">
-                <input type="text" name="name" placeholder="Your name" required>
-                <input type="email" name="email" placeholder="Your email" required>
-                <textarea name="message" placeholder="Your message here"></textarea>
-                <button type="submit">Send</button>
-            </form>"""
-    with st.container():
-        
-        st.markdown('#### Get In Touch With Me! :mailbox:')
-        
-        st.markdown('####')
-        x1 , x2= st.columns((2,2))
-        with x2:
-            
-            st.markdown(contact_form, unsafe_allow_html=True)
-    
-
-    # Use Local CSS File
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    local_css("style/style.css")
-
-
-    with x1:
-
-        lottie_contact = load_lottieurl('https://assets9.lottiefiles.com/packages/lf20_px0ntw70.json')
-        st_lottie(
-        lottie_contact ,
-        speed=1,
-        reverse=False,
-        loop=True,
-        quality="low", # medium ; high
-        height=300,
-        width=600,
-        key=None,)
-        
-
-
-
-        
-
-
-
-
-
-
 
